9c5372a83713f948492902dac5cdacc832f3c7f8
Philip Langdale <philipl@overt.org>
2005-12-29 08:27:51 +0000

    2005-12-29  Philip Langdale  <philipl@mail.utexas.edu>
    	* gphotofs.c: Lots of stuff.
    	- (struct OpenFile) (freeOpenFile) (gphotofs_open)
    	  (gphotofs_release). Add a count for open files so
    	  that they aren't closed too early.
    	- (gpresultToErrno). Map libgphoto2 errors to errnos,
    	  or at least try to. Prevents things going off the
    	  deepend if there is an error.
            - (gphotofs_getattr). Don't go into an infinite loop
              if called for a non-existent file.
            - (gphotofs_readdir). Set the block count for files.
              This allows du to report a meaningful result.
    
    
    git-svn-id: https://svn.code.sf.net/p/gphoto/code/trunk/gphotofs@8354 67ed7778-7388-44ab-90cf-0a291f65f57c

--- gphotofs.c
+++ gphotofs.c
@@ -18,6 +18,48 @@
 #include <errno.h>
 #include <fcntl.h>
 
+struct OpenFile {
+   CameraFile *file;
+   guint count;
+};
+typedef struct OpenFile OpenFile;
+
+static void
+freeOpenFile(OpenFile *openFile)
+{
+   gp_file_unref(openFile->file);
+   g_free(openFile);
+}
+
+static int
+gpresultToErrno(int result)
+{
+   switch (result) {
+   case GP_ERROR_CAMERA_BUSY:
+      return -EBUSY;
+   case GP_ERROR_FILE_NOT_FOUND:
+   case GP_ERROR_DIRECTORY_NOT_FOUND:
+      return -ENOENT;
+   case GP_ERROR_FILE_EXISTS:
+   case GP_ERROR_DIRECTORY_EXISTS:
+      return -EEXIST;
+   case GP_ERROR_PATH_NOT_ABSOLUTE:
+      return -ENOTDIR;
+   case GP_ERROR_CORRUPTED_DATA:
+      return -EIO;
+   case GP_ERROR_CANCEL:
+      return -ECANCELED;
+
+   /* These are pretty dubious mappings. */
+   case GP_ERROR_MODEL_NOT_FOUND:
+      return -EPROTO;
+   case GP_ERROR_CAMERA_ERROR:
+      return -EPERM;
+   case GP_ERROR_OS_FAILURE:
+      return -EPIPE;
+   }
+}
+
 struct GPCtx {
    Camera *camera;
    GPContext *context;
@@ -31,15 +73,16 @@ struct GPCtx {
 typedef struct GPCtx GPCtx;
 
 static int
-gphoto_readdir(const char *path,
-               void *buf,
-               fuse_fill_dir_t filler,
-               off_t offset,
-               struct fuse_file_info *fi)
+gphotofs_readdir(const char *path,
+                 void *buf,
+                 fuse_fill_dir_t filler,
+                 off_t offset,
+                 struct fuse_file_info *fi)
 {
    GPCtx *p;
    CameraList *list;
    int i;
+   int ret = 0;
 
    p = (GPCtx *)fuse_get_context()->private_data;
 
@@ -49,7 +92,10 @@ gphoto_readdir(const char *path,
    /* Read directories */
    gp_list_new(&list);
 
-   gp_camera_folder_list_folders(p->camera, path, list, p->context);
+   ret = gp_camera_folder_list_folders(p->camera, path, list, p->context);
+   if (ret != 0) {
+      goto error;
+   }
 
    for (i = 0; i < gp_list_count(list); i++) {
       struct stat *stbuf;
@@ -57,7 +103,8 @@ gphoto_readdir(const char *path,
       gchar *key;
 
       stbuf = g_new0(struct stat, 1);
-      stbuf->st_mode = S_IFDIR | 0755;
+      stbuf->st_mode = S_IFDIR | 0555;
+      /* This is not a correct number in general. */
       stbuf->st_nlink = 2;
 
       gp_list_get_name(list, i, &name);
@@ -72,7 +119,10 @@ gphoto_readdir(const char *path,
    gp_list_free(list);
    gp_list_new(&list);
 
-   gp_camera_folder_list_files(p->camera, path, list, p->context);
+   ret = gp_camera_folder_list_files(p->camera, path, list, p->context);
+   if (ret != 0) {
+      goto error;
+   }
 
    for (i = 0; i < gp_list_count(list); i++) {
       struct stat *stbuf;
@@ -81,13 +131,18 @@ gphoto_readdir(const char *path,
       CameraFileInfo info;
 
       gp_list_get_name(list, i, &name);
-      gp_camera_file_get_info(p->camera, path, name, &info, p->context);
+      ret = gp_camera_file_get_info(p->camera, path, name, &info, p->context);
+      if (ret != 0) {
+         goto error;
+      }
 
       stbuf = g_new0(struct stat, 1);
       stbuf->st_mode = S_IFREG | 0444;
       stbuf->st_nlink = 1;
       stbuf->st_size = info.file.size;
       stbuf->st_mtime = info.file.mtime;
+      stbuf->st_blocks = (info.file.size / 512) +
+                         (info.file.size % 512 > 0 ? 1 : 0);
 
       filler(buf, name, stbuf, 0);
 
@@ -96,7 +151,10 @@ gphoto_readdir(const char *path,
       g_hash_table_replace(p->files, key, stbuf);
    }
 
-   return 0;
+   return ret;
+
+ error:
+   return gpresultToErrno(ret);
 }
 
 static int dummyfiller(void *buf,
@@ -109,99 +167,114 @@ static int dummyfiller(void *buf,
 
 
 static int
-gphoto_getattr(const char *path,
-               struct stat *stbuf)
+gphotofs_getattr(const char *path,
+                 struct stat *stbuf)
 {
-   int res = 0;
-
+   int ret = 0;
    GPCtx *p = (GPCtx *)fuse_get_context()->private_data;
 
    memset(stbuf, 0, sizeof(struct stat));
 
-    if(strcmp(path, "/") == 0) {
-        stbuf->st_mode = S_IFDIR | 0755;
-        stbuf->st_nlink = 2;
-    } else {
-        gboolean retryLookup;
-        struct stat *mystbuf = NULL;
-        gpointer value;
+   if(strcmp(path, "/") == 0) {
+      stbuf->st_mode = S_IFDIR | 0555;
+      stbuf->st_nlink = 2;
+   } else {
+      struct stat *mystbuf = NULL;
+      gpointer value;
+      guint i;
 
-        do {
-           retryLookup = FALSE;
+      /*
+       * Due to the libgphoto2 api, the best way of verifying
+       * if a file exists is to iterate the contents of that
+       * directory; so if we don't know about the file already,
+       * turn around and call readdir on the directory and try
+       * again.
+       */
+      for (i = 2; i > 0; i--) {
+         gchar *dir;
+         value = g_hash_table_lookup(p->files, path);
+         if (!value) {
+            value = g_hash_table_lookup(p->dirs, path);
+         }
+         if (value) {
+            mystbuf = (struct stat *)value;
+            break;
+         }
 
-           value = g_hash_table_lookup(p->dirs, path);
-           if (value) {
-              mystbuf = (struct stat *)value;
-           } else {
-              value = g_hash_table_lookup(p->files, path);
-              if (value) {
-                 mystbuf = (struct stat *)value;
-              } else {
-                 gchar *dir = g_path_get_dirname(path);
-                 gphoto_readdir(dir, NULL, dummyfiller, 0, NULL);
-                 g_free(dir);
+         dir = g_path_get_dirname(path);
+         ret = gphotofs_readdir(dir, NULL, dummyfiller, 0, NULL);
+         g_free(dir);
+         if (ret != 0) {
+            return ret;
+         }
+      }
 
-                 retryLookup = TRUE;
-              }
-           }
-        } while (retryLookup);
-
-        if (mystbuf) {
-           stbuf->st_mode = mystbuf->st_mode;
-           stbuf->st_nlink = mystbuf->st_nlink;
-           stbuf->st_size = mystbuf->st_size;
-           stbuf->st_mtime = mystbuf->st_mtime;
-        } else {
-           res = -ENOENT;
-        }
-    }
-    return res;
+      if (mystbuf) {
+         stbuf->st_mode = mystbuf->st_mode;
+         stbuf->st_nlink = mystbuf->st_nlink;
+         stbuf->st_size = mystbuf->st_size;
+         stbuf->st_mtime = mystbuf->st_mtime;
+         stbuf->st_blocks = mystbuf->st_blocks;
+      } else {
+         ret = -ENOENT;
+      }
+   }
+   return ret;
 }
 
 static int
-gphoto_open(const char *path,
-            struct fuse_file_info *fi)
+gphotofs_open(const char *path,
+              struct fuse_file_info *fi)
 {
    GPCtx *p = (GPCtx *)fuse_get_context()->private_data;
-   CameraFile *cFile;
+   OpenFile *openFile;
 
    if((fi->flags & 3) != O_RDONLY) {
       return -EACCES;
    }
 
-   cFile = g_hash_table_lookup(p->reads, path);
-   if (!cFile) {
+   openFile = g_hash_table_lookup(p->reads, path);
+   if (!openFile) {
       gchar *dir = g_path_get_dirname(path);
       gchar *file = g_path_get_basename(path);
+      CameraFile *cFile;
+      int ret;
 
       gp_file_new(&cFile);
-      gp_camera_file_get(p->camera, dir, file, GP_FILE_TYPE_NORMAL,
-                         cFile, p->context);
-
-      g_hash_table_replace(p->reads, g_strdup(path), cFile);
-
+      ret = gp_camera_file_get(p->camera, dir, file, GP_FILE_TYPE_NORMAL,
+                               cFile, p->context);
       g_free(file);
       g_free(dir);
+
+      if (ret != 0) {
+         return gpresultToErrno(ret);
+      }
+
+      openFile = g_new0(OpenFile, 1);
+      openFile->file = cFile;
+      openFile->count = 1;
+      g_hash_table_replace(p->reads, g_strdup(path), openFile);
+   } else {
+      openFile->count++;
    }
 
    return 0;
 }
 
 static int
-gphoto_read(const char *path,
-            char *buf, size_t size,
-            off_t offset,
-            struct fuse_file_info *fi)
+gphotofs_read(const char *path,
+              char *buf, size_t size,
+              off_t offset,
+              struct fuse_file_info *fi)
 {
    GPCtx *p = (GPCtx *)fuse_get_context()->private_data;
-   CameraFile *cFile;
+   OpenFile *openFile;
    const char *data;
    unsigned long int dataSize;
 
-   cFile = g_hash_table_lookup(p->reads, path);
-   gp_file_get_data_and_size(cFile, &data, &dataSize);
-
-   if (offset < dataSize) {
+   openFile = g_hash_table_lookup(p->reads, path);
+   if (gp_file_get_data_and_size(openFile->file, &data, &dataSize) == 0 &&
+       offset < dataSize) {
       if (offset + size > dataSize) {
          size = dataSize - offset;
       }
@@ -214,45 +287,45 @@ gphoto_read(const char *path,
 }
 
 static int
-gphoto_release(const char *path,
-               struct fuse_file_info *fi)
+gphotofs_release(const char *path,
+                 struct fuse_file_info *fi)
 {
    GPCtx *p = (GPCtx *)fuse_get_context()->private_data;
+   OpenFile *openFile = g_hash_table_lookup(p->reads, path);
 
-   g_hash_table_remove(p->reads, path);
+   openFile->count--;
+   if (openFile->count == 0) {
+      g_hash_table_remove(p->reads, path);
+   }
 
    return 0;
 }
 
 static int
-gphoto_unlink(const char *path)
+gphotofs_unlink(const char *path)
 {
    GPCtx *p = (GPCtx *)fuse_get_context()->private_data;
    gchar *dir = g_path_get_dirname(path);
    gchar *file = g_path_get_basename(path);
    int ret;
 
-   
-
-   ret = gp_camera_file_delete(p->camera, dir, file, p->context);
-   if (ret == 0) {
-      g_hash_table_remove(p->files, path);
-   } else {
-      switch (ret) {
-      case GP_ERROR_FILE_NOT_FOUND:
-         ret = -ENOENT;
-         break;
-      default:
-         ret = -EPROTO;
-         break;
-      }
+   /* Don't allow deletion of open files. */
+   if (g_hash_table_lookup(p->reads, path)) {
+      return -EBUSY;
    }
 
-   return ret;
+   ret = gp_camera_file_delete(p->camera, dir, file, p->context);
+   if (!ret) {
+      return gpresultToErrno(ret);
+   }
+
+   g_hash_table_remove(p->files, path);
+
+   return 0;
 }
 
 static void *
-gphoto_init(void)
+gphotofs_init(void)
 {
    GPCtx *p = g_new0(GPCtx, 1);
 
@@ -264,13 +337,13 @@ gphoto_init(void)
    p->files = g_hash_table_new_full(g_str_hash, g_str_equal,
                                     g_free, g_free);
    p->reads = g_hash_table_new_full(g_str_hash, g_str_equal,
-                                    g_free, (GDestroyNotify)gp_file_unref);
+                                    g_free, (GDestroyNotify)freeOpenFile);
 
    return p;
 }
 
 static void
-gphoto_destroy(void *context)
+gphotofs_destroy(void *context)
 {
    if (!context) {
       return;
@@ -301,18 +374,18 @@ gphoto_destroy(void *context)
    g_free(p);
 }
 
-static struct fuse_operations gphoto_oper = {
-    .init	= gphoto_init,
-    .destroy	= gphoto_destroy,
-    .readdir	= gphoto_readdir,
-    .getattr	= gphoto_getattr,
-    .open	= gphoto_open,
-    .read	= gphoto_read,
-    .release	= gphoto_release,
-    .unlink	= gphoto_unlink,
+static struct fuse_operations gphotofs_oper = {
+    .init	= gphotofs_init,
+    .destroy	= gphotofs_destroy,
+    .readdir	= gphotofs_readdir,
+    .getattr	= gphotofs_getattr,
+    .open	= gphotofs_open,
+    .read	= gphotofs_read,
+    .release	= gphotofs_release,
+    .unlink	= gphotofs_unlink,
 };
 
 int main(int argc, char *argv[])
 {
-    return fuse_main(argc, argv, &gphoto_oper);
+    return fuse_main(argc, argv, &gphotofs_oper);
 }
